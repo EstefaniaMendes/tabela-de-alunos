e5a75dc808794dcdd8c3708687e1c830fb215ef9
Fany <47598212+EstefaniaMendes@users.noreply.github.com>
2019-12-04 12:54:27 -0300

    update

---
+++ src/components/aluno.js
@@ -0,0 +1,122 @@
+import React from 'react'
+
+class Alunos extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state={
+            alunos: [
+
+                {
+                nome: 'Estefania',
+                polo: 'Prazeres',
+                Idade: 22,
+                },
+
+                {
+                nome: 'Rhuan',
+                polo: 'Prazeres',
+                Idade: 20,
+                }, 
+
+                {
+                nome: 'Geysa',
+                polo: 'Prazeres',
+                Idade: 20,
+                },
+
+                {
+                nome: 'Caroline',
+                polo: 'Prazeres',
+                Idade: 19,
+                }, 
+
+                {
+                nome: 'renan',
+                polo: 'Prazeres',
+                Idade: 14,
+                },
+
+                {
+                nome: 'zaira',
+                polo: 'Prazeres',
+                Idade: 19,
+                },
+
+                {
+                nome: 'cicero',
+                polo: 'Prazeres',
+                Idade: 24,
+                },
+
+                {
+                nome: 'cassio',
+                polo: 'Prazeres',
+                Idade: 21,
+                },
+
+                {
+                nome: 'Priscila',
+                polo: 'Prazeres',
+                Idade: 32,
+                },
+
+                {
+                nome: 'maicon',
+                polo: 'Complexo do alemão',
+                Idade: 23,
+                },
+
+                {
+                nome: 'Jorge',
+                polo: 'Complexo do alemão',
+                Idade: 58,
+                },
+
+                {
+                nome: 'Dandara',
+                polo: 'Complexo do alemão',
+                Idade: 21,
+                },
+
+                {
+                nome: 'lucas',
+                polo: 'Complexo do alemão',
+                Idade: 24,
+                },
+
+                {
+                nome: 'douglas',
+                polo: 'Complexo do alemão',
+                Idade: 24,
+                },
+
+                {
+                nome: 'cleiton',
+                polo: 'Complexo do alemão',
+                Idade: 19,
+                },
+
+                {
+                nome: 'luan',
+                polo: 'Complexo do alemão',
+                Idade: 25,
+                },
+
+                {
+                nome: 'Artun',
+                polo: 'Complexo do alemão',
+                Idade: 24,
+                },
+
+                {
+                nome: 'victo',
+                polo: 'Complexo do alemão',
+                Idade: 24,
+                },
+
+                ]
+        }
+    }
+}
+export default Alunos;
